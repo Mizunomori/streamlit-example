993db2c5692783b0c56c138db8e4a0ec25434e18
Mizunomori <hhh58@cornell.edu>
2022-02-09 15:46:16 -0500

    Changed to hw requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 from collections import namedtuple
+from tkinter import Image
 import altair as alt
 import math
-import pandas as pd
+import mkl
+mkl.domain_set_num_threads(1, domain='fft')
 import streamlit as st
+import numpy as np
+import time 
+import pandas as pd
 
 """
 # Welcome to Streamlit!
@@ -15,24 +20,83 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+st.title("SYSEN 5150 HW 1")
+st.subheader("Demonstration of data elements") 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.metric(label="Price", value= "$3.50" ,delta=None, delta_color="normal") 
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+df = pd.DataFrame( 
+    np.random.randn(17,7),columns=('col %d' % i for i in range(7))
+) 
 
-    points_per_turn = total_points / num_turns
+st.table(df) 
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+chart_data = pd.DataFrame(
+     np.random.randn(27, 4),
+     columns=['a', 'b', 'c', 'd'])
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+with st.container():
+    st.line_chart(chart_data) 
+
+with st.expander(label="Area Chart", expanded= True):
+    st.area_chart(chart_data) 
+
+
+inverter = st.checkbox('Inverter Level')
+
+if inverter: 
+    st.write('Inverter data') 
+
+
+
+st.image(image = "https://www.theventuremag.com/wp-content/uploads/2018/10/iStock-961433280.jpg", caption='Solar array')
+
+video_file = open('star.mp4', 'rb') 
+video_bytes = video_file.read() 
+st.video(video_bytes) 
+
+bar = st.progress(0) 
+for percent_complete in range(100): 
+    time.sleep(0.1)
+    bar.progress(percent_complete+1) 
+
+st.error('Invalid site name')
+
+with st.form("Site Info"):
+    
+    slider_val = st.slider("Number of Inverters")
+    checkbox_val = st.checkbox("MPPT")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+
+#st.experimental_rerun() 
+
+the_table = pd.DataFrame(
+    np.random.randn(2, 4),
+    columns=('col %d' % i for i in range(4)))
+
+df2 = pd.DataFrame(
+    np.random.randn(2, 4),
+    columns=('col %d' % i for i in range(4)))
+the_table.add_rows(df2) 
+
+st.experimental_show(the_table) 
+
+st.help(np.matrix) 
+
+@st.experimental_memo
+def fetch_and_clean_data(url = "https://docs.google.com/spreadsheets/d/1vy0pSDILrhLdsFSS9gaETO1iVAgvKC1R/edit?rtpof=true"):
+     # Fetch data from URL here, and then clean it up.
+        return data
+
+d1 = fetch_and_clean_data(url = "https://docs.google.com/spreadsheets/d/1vy0pSDILrhLdsFSS9gaETO1iVAgvKC1R/edit#gid=1245828365")
+# Actually executes the function, since this is the first time it was
+# encountered.
+
+if st.button("Clear All"):
+    # Clear values from *all* memoized functions:
+    # i.e. clear values from both square and cube
+    st.experimental_memo.clear()
